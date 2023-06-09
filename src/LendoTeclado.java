c16c2fb3df1acca409c4e8ef17734127f72154aa
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-09 19:42:09 -0300

    add: operadores aritmeticos

--- src/LendoTeclado.java
+++ src/LendoTeclado.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 
 public class LendoTeclado {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Digite seu nome: ");
+            String nome = scan.nextLine(); // Ler a linha completa
 
-        System.out.println("Digite seu nome: ");
-        String nome = scan.nextLine(); // Ler a linha completa
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = scan.next(); // Ler apenas a primeira palavra
 
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = scan.next(); // Ler apenas a primeira palavra
+            System.out.println("Digite sua idade: ");
+            int idade = scan.nextInt(); // Ler um número inteiro
 
-        System.out.println("Digite sua idade: ");
-        int idade = scan.nextInt(); // Ler um número inteiro
-
-        System.out.println("Digite sua altura: ");
-        double altura = scan.nextDouble(); // Ler um número double
+            System.out.println("Digite sua altura: ");
+            double altura = scan.nextDouble(); // Ler um número double
 
 
-        System.out.println("IFORMAÇÕES:");
-        System.out.println("NOME: " + nome + " " + sobrenome);
-        System.out.println("IDADE: " + idade + " anos");
-        System.out.println("ALTURA: " + altura);
+            System.out.println("IFORMAÇÕES:");
+            System.out.println("NOME: " + nome + " " + sobrenome);
+            System.out.println("IDADE: " + idade + " anos");
+            System.out.println("ALTURA: " + altura);
+        }
     }
 }
---
+++ src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+    public static void main(String[] args) {
+
+        // Operadores aritméticos
+        int n1 = 2;
+        int n2 = 4;
+        
+        int resultadoAdicao = n1 + n2;
+        int resultadoSubtracao = n1 - n2;
+        int resultadoMultiplicacao = n1 * n2;
+        int resultadoDivisao = n1 / n2;
+
+        System.out.println("ADIÇÃO: " + resultadoAdicao);
+        System.out.println("SUBTRAÇÃO: " + resultadoSubtracao);
+        System.out.println("MULTIPLICAÇÃO: " + resultadoMultiplicacao);
+        System.out.println("DIVISÃO: " + resultadoDivisao);
+
+        
+    }
+}
