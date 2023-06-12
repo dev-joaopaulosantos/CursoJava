dd194d7fbc6d4df16153f20e6ed04afcfa217238
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-11 21:52:07 -0300

    add: exercicio 02

---
+++ src/br/com/joaopaulo/labs/Exer02.java
@@ -0,0 +1,14 @@
+package br.com.joaopaulo.labs;
+
+import java.util.Scanner;
+
+public class Exer02 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Digite um número: ");
+            double num = scan.nextDouble();
+
+            System.out.println("O número digitado foi: " + num);
+        }
+    }
+}
