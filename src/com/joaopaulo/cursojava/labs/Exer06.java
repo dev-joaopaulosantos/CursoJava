cfd3556187716d73d2f7a6ace9ba1af55b0ec6ce
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-11 23:05:45 -0300

    add: exercicio 06

---
+++ src/com/joaopaulo/cursojava/labs/Exer06.java
@@ -0,0 +1,20 @@
+package com.joaopaulo.cursojava.labs;
+
+import java.util.Scanner;
+
+public class Exer06 {
+    public static void main(String[] args) {
+        System.out.println("CAULCULADORA DE ÁREA DE UM CÍRCULO");
+
+        try (Scanner scan = new Scanner(System.in)) {
+            
+            System.out.print("Digite o raio do círculo: ");
+            double raio = scan.nextDouble();
+            double pi = 3.14;
+            double area = pi * Math.pow(raio, 2);
+
+            System.out.println("A área do circulo em cm é: " + area + "cm²");
+        }
+
+    }
+}
