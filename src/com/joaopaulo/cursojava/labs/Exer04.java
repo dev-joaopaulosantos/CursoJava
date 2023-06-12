4e2079e1eaaf4c6cb71595d37e1ffe3a64d3f2ef
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-11 22:38:39 -0300

    add: exercicio 06

--- src/com/joaopaulo/cursojava/labs/Exer04.java
+++ src/com/joaopaulo/cursojava/labs/Exer04.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class Exer04 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("CALCULADORA DE MÉDIA BIMESTRAL");
 
-        System.out.println("CALCULADORA DE MÉDIA BIMESTRAL");
+            System.out.println("Digite sua 1ª nota: ");
+            double nota1 = scan.nextDouble();
 
-        System.out.println("Digite sua 1ª nota: ");
-        double nota1 = scan.nextDouble();
+            System.out.println("Digite sua 2ª nota: ");
+            double nota2 = scan.nextDouble();
 
-        System.out.println("Digite sua 2ª nota: ");
-        double nota2 = scan.nextDouble();
+            System.out.println("Digite sua 3ª nota: ");
+            double nota3 = scan.nextDouble();
 
-        System.out.println("Digite sua 3ª nota: ");
-        double nota3 = scan.nextDouble();
+            System.out.println("Digite sua 4ª nota: ");
+            double nota4 = scan.nextDouble();
 
-        System.out.println("Digite sua 4ª nota: ");
-        double nota4 = scan.nextDouble();
-
-        double media = (nota1 + nota2 + nota3 + nota4) / 4;
-        System.out.println("Sua média bimestral é: " + media);
+            double media = (nota1 + nota2 + nota3 + nota4) / 4;
+            System.out.println("Sua média bimestral é: " + media);
+        }
     }
 }
