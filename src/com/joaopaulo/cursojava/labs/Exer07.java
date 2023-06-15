6a968fda948b6d320b08f42caac1fe281ee56bda
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-14 23:10:40 -0300

    refactor

--- src/com/joaopaulo/cursojava/labs/Exer07.java
+++ src/com/joaopaulo/cursojava/labs/Exer07.java
@@ -8,13 +8,13 @@ public class Exer07 {
 
         try (Scanner scan = new Scanner(System.in)) {
 
-            System.out.print("Digite o lado do quadrado: ");
+            System.out.print("Digite o lado do quadrado em cm: ");
             double lado = scan.nextDouble();
 
             double area = Math.pow(lado, 2);
 
-            System.out.println("Area do quadrado: " + area);
-            System.out.println("Dobro da area do quadrado: " + (area * 2));
+            System.out.println("Area do quadrado: " + area + "cm²");
+            System.out.println("Dobro da area do quadrado: " + (area * 2) + "cm²");
         }
     }
 }
