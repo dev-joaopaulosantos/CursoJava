28d55bf74feeb621e797002407c087f58aa2575b
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-15 23:52:38 -0300

    add: exercicio 10

---
+++ src/com/joaopaulo/cursojava/labs/Exer10.java
@@ -0,0 +1,18 @@
+package com.joaopaulo.cursojava.labs;
+
+import java.util.Scanner;
+
+public class Exer10 {
+    public static void main(String[] args) {
+        System.out.println("*** CONVERSOR DE TEMPERATURA ***");
+
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Digite a temperatura em celsius: ");
+            double celsius = scan.nextDouble();
+
+            double farenheit = celsius * 1.8 + 32;
+
+            System.out.println(celsius + " °C = " + farenheit + " °F")
+        };
+    }
+}
