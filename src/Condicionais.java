d3566ff2360843e09a6af7b2dcf093b271f0026e
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-18 18:33:51 -0300

    add: condicional switch case

--- src/Condicionais.java
+++ src/Condicionais.java
@@ -4,6 +4,7 @@ public class Condicionais {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Condicionais IF / ELSE
         System.out.print("Digite sua idade: ");
         int idade = scan.nextInt();
 
@@ -11,6 +12,36 @@ public class Condicionais {
             System.out.println("Você é maior de idade!");
         } else {
             System.out.println("Você não é maior de idade!");
+        }
+
+        // Condicional SWITCH CASE
+        System.out.print("Digite um dia da semana. 1-7: ");
+        int diaSemana = scan.nextInt();
+
+        switch (diaSemana) {
+            case 1:
+                System.out.println("Segunda!");
+                break;
+            case 2:
+                System.out.println("Terça!");
+                break;
+            case 3:
+                System.out.println("Quarta!");
+                break;
+            case 4:
+                System.out.println("Quinta!");
+                break;
+            case 5:
+                System.out.println("Sexta!");
+                break;
+            case 6:
+                System.out.println("Sábado!");
+                break;
+            case 7:
+                System.out.println("Domingo!");
+                break;
+            default:
+                System.out.println("Dia inválido!");
         }
     }
 }
