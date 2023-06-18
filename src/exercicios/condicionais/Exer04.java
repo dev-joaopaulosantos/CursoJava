4263867e5582f069e45552b1e4ffd8a8e28cb081
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-18 20:42:45 -0300

    add: exercicio 04

---
+++ src/exercicios/condicionais/Exer04.java
@@ -0,0 +1,22 @@
+package exercicios.condicionais;
+
+import java.util.Scanner;
+
+public class Exer04 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Digite uma letra: ");
+            String letra = scan.next();
+
+            if (letra.equalsIgnoreCase("a") ||
+                    letra.equalsIgnoreCase("e") ||
+                    letra.equalsIgnoreCase("i") ||
+                    letra.equalsIgnoreCase("o") ||
+                    letra.equalsIgnoreCase("u")) {
+                System.out.println("A letra digitada é uma VOGAL");
+            } else {
+                System.out.println("A letra digitada é uma CONSOANTE");
+            }
+        }
+    }
+}
