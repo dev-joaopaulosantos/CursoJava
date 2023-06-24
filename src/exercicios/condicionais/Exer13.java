b72145bc3e1ffad49ade9345ba9a19111fa32217
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-23 23:05:22 -0300

    add: exercicio 13

--- src/exercicios/condicionais/Exer13.java
+++ src/exercicios/condicionais/Exer13.java
@@ -4,41 +4,41 @@ import java.util.Scanner;
 
 public class Exer13 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.print("Digite um número correspondente a um dia da semana: ");
+            int numDia = scan.nextInt();
 
-        System.out.print("Digite um número correspondente a um dia da semana: ");
-        int numDia = scan.nextInt();
+            String dia = "";
 
-        String dia = "";
+            switch (numDia) {
+                case 1:
+                    dia = "Domingo";
+                    break;
+                case 2:
+                    dia = "Segunda Feira";
+                    break;
+                case 3:
+                    dia = "Terça Feira";
+                    break;
+                case 4:
+                    dia = "Quarta Feira";
+                    break;
+                case 5:
+                    dia = "Quinta Feira";
+                    break;
+                case 6:
+                    dia = "Sexta Feira";
+                    break;
+                case 7:
+                    dia = "Sábado";
+                    break;
+                default:
+                    dia = "Número Inválido!";
+                    break;
+            }
 
-        switch (numDia) {
-            case 1:
-                dia = "Domingo";
-                break;
-            case 2:
-                dia = "Segunda Feira";
-                break;
-            case 3:
-                dia = "Terça Feira";
-                break;
-            case 4:
-                dia = "Quarta Feira";
-                break;
-            case 5:
-                dia = "Quinta Feira";
-                break;
-            case 6:
-                dia = "Sexta Feira";
-                break;
-            case 7:
-                dia = "Sábado";
-                break;
-            default:
-                dia = "Número Inválido!";
-                break;
+            System.out.println("Dia: " + dia);
         }
-
-        System.out.println("Dia: " + dia);
 
     }
 }
