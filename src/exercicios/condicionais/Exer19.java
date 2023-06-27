59a18b9896a3549436f351745c94e054ec4dae0d
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-27 02:04:56 -0300

    add: exercicio 19

--- src/exercicios/condicionais/Exer19.java
+++ src/exercicios/condicionais/Exer19.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Exer19 {
     public static void main(String[] args) {
         try (Scanner scan = new Scanner(System.in)) {
+            
             System.out.print("Digite o 1° número: ");
             int num1 = scan.nextInt();
 
