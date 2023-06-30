7fc826c2033d3ba8e9b0ed81b18834964427ec06
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-06-29 22:58:10 -0300

    add: exercicio 22

---
+++ src/exercicios/condicionais/Exer22.java
@@ -0,0 +1,9 @@
+package exercicios.condicionais;
+
+import java.util.Scanner;
+
+public class Exer22 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
