386fd7b2f66ebc7806b468e20a66dcd9962cc6cc
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-07-04 00:43:02 -0300

    add in loops: exercicio 03

--- src/exercicios/loops/Exer03.java
+++ src/exercicios/loops/Exer03.java
@@ -5,55 +5,57 @@ import java.util.Scanner;
 public class Exer03 {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        String nome;
-        String sexo;
-        String estadoCivil;
-        double salario;
-        int idade;
+        try (Scanner scan = new Scanner(System.in)) {
+            
+            String nome;
+            String sexo;
+            String estadoCivil;
+            double salario;
+            int idade;
 
-        boolean dadosValidos = false;
+            boolean dadosValidos = false;
 
-        do {
-            System.out.print("Nome: ");
-            nome = scan.next();
+            do {
+                System.out.print("Nome: ");
+                nome = scan.next();
 
-            System.out.print("Idade: ");
-            idade = scan.nextInt();
+                System.out.print("Idade: ");
+                idade = scan.nextInt();
 
-            System.out.print("Salário: ");
-            salario = scan.nextDouble();
+                System.out.print("Salário: ");
+                salario = scan.nextDouble();
 
-            System.out.print("Sexo: ");
-            sexo = scan.next();
+                System.out.print("Sexo: ");
+                sexo = scan.next();
 
-            System.out.print("Estado Civil: ");
-            estadoCivil = scan.next();
+                System.out.print("Estado Civil: ");
+                estadoCivil = scan.next();
 
-            if ((nome.length() > 3) && (idade > 0 && idade < 150) && (salario > 0)) {
-                if ((sexo.equalsIgnoreCase("F") || sexo.equalsIgnoreCase("M"))) {
-                    if (estadoCivil.equalsIgnoreCase("S") || estadoCivil.equalsIgnoreCase("C")
-                            || estadoCivil.equalsIgnoreCase("V") || estadoCivil.equalsIgnoreCase("D")) {
-                        dadosValidos = true;
-                        System.out.println("+---------------------------+");
-                        System.out.println("         INFORMAÇÕES         ");
-                        System.out.println("+---------------------------+");
-                        System.out.println("Nome: " + nome);
-                        System.out.println("-----------------------------");
-                        System.out.println("Idade: " + idade);
-                        System.out.println("-----------------------------");
-                        System.out.println("Salário: " + salario);
-                        System.out.println("-----------------------------");
-                        System.out.println("Sexo: " + sexo);
-                        System.out.println("-----------------------------");
-                        System.out.println("Estado Civil: " + estadoCivil);
-                        System.out.println("+---------------------------+");
+                if ((nome.length() > 3) && (idade > 0 && idade < 150) && (salario > 0)) {
+                    if ((sexo.equalsIgnoreCase("F") || sexo.equalsIgnoreCase("M"))) {
+                        if (estadoCivil.equalsIgnoreCase("S") || estadoCivil.equalsIgnoreCase("C")
+                                || estadoCivil.equalsIgnoreCase("V") || estadoCivil.equalsIgnoreCase("D")) {
+                            dadosValidos = true;
+                            System.out.println("+---------------------------+");
+                            System.out.println("         INFORMAÇÕES         ");
+                            System.out.println("+---------------------------+");
+                            System.out.println("Nome: " + nome);
+                            System.out.println("-----------------------------");
+                            System.out.println("Idade: " + idade);
+                            System.out.println("-----------------------------");
+                            System.out.println("Salário: " + salario);
+                            System.out.println("-----------------------------");
+                            System.out.println("Sexo: " + sexo);
+                            System.out.println("-----------------------------");
+                            System.out.println("Estado Civil: " + estadoCivil);
+                            System.out.println("+---------------------------+");
+                        }
+                    } else {
+                        System.out.println("Dados Inválidos!");
                     }
-                } else {
-                    System.out.println("Dados Inválidos!");
-                }
 
-            }
-        } while (!dadosValidos);
+                }
+            } while (!dadosValidos);
+        }
     }
 }
