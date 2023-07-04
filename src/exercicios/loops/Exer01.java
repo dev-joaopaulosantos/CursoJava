d15a65b1889b27c4fb8dc92c961f4e82a1393b02
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-07-03 21:29:53 -0300

    add in loops: exercicio 01

---
+++ src/exercicios/loops/Exer01.java
@@ -0,0 +1,20 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+public class Exer01 {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            
+            System.out.print("Digite um número de 0 a 10: ");
+            int num = scan.nextInt();
+
+            while (num < 0 || num > 10) {
+                System.out.print("Número inválido! Digite um número de 0 a 10: ");
+                num = scan.nextInt();
+            }
+        }
+
+        System.out.println("Você digitou um número valido!");
+    }
+}
