ca54647c2170e571eaf871f8292b6ebcca48e90b
Joao Paulo Santos <joao.p.santos.silva22@gmail.com>
2023-07-27 20:52:54 -0300

    add in loops: exercicio 06

---
+++ src/exercicios/loops/Exer06.java
@@ -0,0 +1,12 @@
+package exercicios.loops;
+
+public class Exer06 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 20; i++) {
+            System.out.print(i + ", ");
+        }
+        System.out.print("FIM!");
+
+    }
+}
